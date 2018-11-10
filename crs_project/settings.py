519876432a68379b7c2b0315787c0963c9041228
James Lee <kidosol3616@gmail.com>
2018-11-10 10:11:13 +0000

    initial commit

--- crs_project/settings.py
+++ crs_project/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'crs_project.urls'
