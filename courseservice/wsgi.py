cb248295a27932fe18413f965719e0c61332afee
James Lee <kidosol3616@gmail.com>
2018-11-10 08:50:57 +0000

    initial commit

--- courseservice/wsgi.py
+++ courseservice/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for courseservice project.
+WSGI config for crs_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'courseservice.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crs_project.settings')
 
 application = get_wsgi_application()
